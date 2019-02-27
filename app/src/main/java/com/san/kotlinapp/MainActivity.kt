ae9685aecc516bae4367920b68a558bce7ca0211
BOse <seint.bo@edenred.com>
2019-02-27 17:32:51 +0800

    added First Fragment Weather Forecast Fragment

--- app/src/main/java/com/san/kotlinapp/MainActivity.kt
+++ app/src/main/java/com/san/kotlinapp/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.san.kotlinapp
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
+import com.san.kotlinapp.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var  mBinding : ActivityMainBinding
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
@@ -27,8 +32,29 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
+        mBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+            replaceFragmentInActivity(WeatherForecastFragment(), R.id.contentFrame, false,null)
+
+    }
+
+
+    fun replaceFragmentInActivity(
+        fragment: Fragment,
+        frameId: Int, addToBacksStack: Boolean, bundle: Bundle?
+    ) {
+
+        //Timber.d("replaceFragmentInActivity count:%s, addToBacksStack:%s",fragmentManager.getBackStackEntryCount(), addToBacksStack);
+        if (!addToBacksStack) {
+            supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+        }
+        val transaction = supportFragmentManager.beginTransaction()
+        fragment.arguments = bundle
+        transaction.replace(frameId, fragment)
+        if (addToBacksStack) {
+            transaction.addToBackStack(null)
+        }
+        transaction.commit()
     }
 }
---
+++ app/src/main/java/com/san/kotlinapp/WeatherForecastViewModel.kt
@@ -0,0 +1,7 @@
+package com.san.kotlinapp
+
+import android.arch.lifecycle.ViewModel;
+
+class WeatherForecastViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
